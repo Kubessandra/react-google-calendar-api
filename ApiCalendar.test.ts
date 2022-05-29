e4f1334d890fa0447361c0749e22441a036e47f2
Kubessandra <aymeric.beudaert@gmail.com>
2022-05-29 14:35:33 +0200

    2.0.0

--- ApiCalendar.test.ts
+++
@@ -1,8 +0,0 @@
-declare var test: any;
-
-import ApiCalendar from './build/ApiCalendar';
-
-test('setCalendar method', () => {
-    ApiCalendar.setCalendar('test-calendar');
-    expect(ApiCalendar.calendar).toBe('test-calendar');
-});
